d589ceeafb8d72c59f865d74c98d266089b169f5
Ahmad Abou Hachem <ahmedaboha@hotmail.com>
2023-08-26 15:40:05 +0200

    13-security and manage the roles with data bank 14-create an error page for the access denied 15-some css and html for the pages 16-writing the roles and the user on top of page

--- src/main/java/com/example/myfirstproject/controller/EmployeeController.java
+++ src/main/java/com/example/myfirstproject/controller/EmployeeController.java
@@ -2,9 +2,6 @@ package com.example.myfirstproject.controller;
 
 import com.example.myfirstproject.entity.Employee;
 import com.example.myfirstproject.service.EmployeeService;
-import com.fasterxml.jackson.databind.annotation.JsonValueInstantiator;
-import jakarta.validation.Valid;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,24 +14,24 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
-    public EmployeeController(EmployeeService theEmployeeService){
-        employeeService=theEmployeeService;
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
 
     @GetMapping("")
-    public String getCompany(){
+    public String getCompany() {
         return "/company/home";
     }
 
     @GetMapping("/employees")
-    public String getEmployees(Model theModel){
+    public String getEmployees(Model theModel) {
         List<Employee> myEmployees = employeeService.findAll();
-        theModel.addAttribute("Employees",myEmployees);
+        theModel.addAttribute("Employees", myEmployees);
         return "/company/employees";
     }
 
     @GetMapping("/form")
-    public String showForm(Model theModel){
+    public String showForm(Model theModel) {
         Employee employee = new Employee();
         theModel.addAttribute("employee", employee);
 
@@ -42,24 +39,37 @@ public class EmployeeController {
     }
 
     @PostMapping("/save")
-    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.save(employee);
         return "redirect:/company/employees";
     }
 
     @GetMapping("/update")
-    public String update(@RequestParam("employeeId")int theId, Model theModel){
-        Employee employee= employeeService.findById(theId);
+    public String update(@RequestParam("employeeId") int theId, Model theModel) {
+        Employee employee = employeeService.findById(theId);
 
-        theModel.addAttribute("employee",employee);
+        theModel.addAttribute("employee", employee);
 
         return "/company/add-employee";
     }
 
-    @GetMapping("delete")
-    public String delete(@RequestParam("employeeId")int theId){
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
         employeeService.deleteById(theId);
         return "redirect:/company/employees";
     }
 
+    @GetMapping("/leaders")
+    public String leaderMeeting(){
+
+        return "/company/meetings";
     }
+
+    @GetMapping("/access")
+    public String accessFailure(){
+
+   return "/company/access";
+    }
+
+
+}
--- src/main/java/com/example/myfirstproject/security/SecurityConfig.java
+++ src/main/java/com/example/myfirstproject/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -16,96 +18,107 @@ import javax.sql.DataSource;
 @Configuration
 public class SecurityConfig {
 
-    /*
-   @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
-
-       JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-       // define query to retrieve a user by username
-       jdbcUserDetailsManager.setUsersByUsernameQuery(
-               "select user_id, pw, active from members where user_id=?");
-
-       // define query to retrieve a user by roll
-       jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-               "select user_id, role from roles where user_id=?");
-
-       return jdbcUserDetailsManager;
-    }
-*/
 
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+
+        // define query to retrieve a user by roll
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+
+   /* @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails ahmadAdmin = User.builder()
+        UserDetails ahmad = User.builder()
                 .username("ahmad")
                 .password("{bcrypt}$2a$10$Y9u2ihG0wp/qBOgoSpAfoulP/QLs8CFMmTnTR1qpA2en4j/iGxOQO")
                 .roles("USER", "MANAGER", "ADMIN")
                 .build();
 
-        UserDetails ahmadManager = User.builder()
+        UserDetails basel = User.builder()
                 .username("basel")
                 .password("{bcrypt}$2a$10$7krRtFMBiyCUstEmQ4jBUOdjoL8egwqTvfOSt0Umhe4dpizpiPhB.")
                 .roles("USER", "MANAGER")
                 .build();
 
-        UserDetails ahmadUser = User.builder()
+        UserDetails najm = User.builder()
                 .username("najm")
                 .password("{bcrypt}$2a$10$QSEAsmiGHmXwffcJgOXJXeZCiCb42mGgmnxGCPRNVoeRsKOIrupbO")
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(ahmadAdmin, ahmadManager, ahmadUser);
+        return new InMemoryUserDetailsManager(ahmad, basel, najm);
     }
-
+*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-      /*  http.authorizeHttpRequests(configurer -> configurer
+   /*     http.authorizeHttpRequests(configurer -> configurer
 
-                //company
-                .requestMatchers(HttpMethod.GET,"/company").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/company/employees").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/company/form").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.GET,"/company/update").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.POST,"/company/save").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.GET,"/company/delete").hasRole("ADMIN")
+                        //company
+                        .requestMatchers(HttpMethod.GET, "/company").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/company/employees").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/company/form").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.GET, "/company/leaders").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.GET, "/company/update").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.POST, "/company/save").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.GET, "/company/delete").hasRole("ADMIN")
 
-                //error
-                .requestMatchers(HttpMethod.GET,"/error").hasRole("USER")
+                        //error
+                        .requestMatchers(HttpMethod.GET, "/error").hasRole("USER")
 
-                //customer
-                .requestMatchers(HttpMethod.GET,"/customer").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/customer/processForm").hasRole("USER")
+                        //customer
+                        .requestMatchers(HttpMethod.GET, "/customer").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/customer/processForm").hasRole("USER")
 
-                //school
-                .requestMatchers(HttpMethod.GET,"/school").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/school/students").hasRole("USER")
+                        //school
+                        .requestMatchers(HttpMethod.GET, "/school").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/school/students").hasRole("USER")
 
-                //home
-                .requestMatchers(HttpMethod.GET,"/home").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/home/time").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/home/coachinfo").hasRole("USER")
+                        //home
+                        .requestMatchers(HttpMethod.GET, "/home").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/home/time").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/home/coachinfo").hasRole("USER")
 
-                //office
-                .requestMatchers(HttpMethod.GET,"/office").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/office/employees").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/office/employees/**").hasRole("USER")
-                .requestMatchers(HttpMethod.POST,"/office/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.PUT,"/office/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.DELETE,"/office/employees/**").hasRole("ADMIN")
-        ); */
-
-        http.authorizeHttpRequests(configurer -> configurer
-                        .anyRequest().authenticated())
-                        .formLogin(form -> form
+                        //office
+                        .requestMatchers(HttpMethod.GET, "/office").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/office/employees").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/office/employees/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/office/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/office/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/office/employees/**").hasRole("ADMIN"))
+                .formLogin(form -> form
                         .loginPage("/login")
                         .loginProcessingUrl("/authenticateTheUser")
                         .permitAll())
-                        .logout(logout->logout.permitAll());
+                .logout(logout -> logout.permitAll())
+                .exceptionHandling(configurer -> configurer.accessDeniedPage("/error"));
+*/
+        http.authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/company/form").hasRole("MANAGER")
+                        .requestMatchers("/company/update").hasRole("MANAGER")
+                        .requestMatchers("/company/save").hasRole("MANAGER")
+                        .requestMatchers("/company/leaders").hasRole("MANAGER")
+                        .requestMatchers("/company/delete").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                         .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll())
+                         .logout(logout -> logout.permitAll())
+                         .exceptionHandling(configurer -> configurer.accessDeniedPage("/company/access"));
 
-        //  http.httpBasic(Customizer.withDefaults());
-
-        // http.csrf(csrf -> csrf.disable());
+     /*   http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());*/
 
         return http.build();
     }
